7b6693803b19807f1a8f7f269eb8f6ccb3b75037
admiralmattbar <mdkissel@gmail.com>
2017-08-10 19:21:11 -0700

    elevator and EnumFacing

--- src/main/java/org/gsa/basemod/blockclasses/BlockClimber.java
+++ src/main/java/org/gsa/basemod/blockclasses/BlockClimber.java
@@ -1,29 +1,36 @@
 package org.gsa.basemod.blockclasses;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockLadder;
+
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-import javax.annotation.Nullable;
 
 
 public class BlockClimber extends Block {
 
+    public static final PropertyDirection FACING = BlockHorizontal.FACING;
 
     protected static final AxisAlignedBB COLLISION_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.9375D, 0.9375D);
     protected static final AxisAlignedBB BOUNDING_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.9375D, 0.9375D);
 
     public BlockClimber() {
+
         super(Material.ROCK);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+
     }
 
     @Override
@@ -39,6 +46,23 @@ public class BlockClimber extends Block {
 
     }
 
+    @Override
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        entityIn.setPosition(entityIn.posX, entityIn.posY + 0.3D, entityIn.posZ);
+
+    }
+
+
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState state) {
+        return super.getRenderType(state);
+    }
+
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
     public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos)
     {
         return COLLISION_AABB;
@@ -49,5 +73,29 @@ public class BlockClimber extends Block {
         return BOUNDING_AABB;
     }
 
+    public int getMetaFromState(IBlockState state) {
+        return ((EnumFacing) state.getValue(FACING)).getHorizontalIndex();
 
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        IBlockState state = super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer);
+        state = state.withProperty(FACING, placer.getHorizontalFacing());
+        return state;
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return this.getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] { FACING });
+    }
 }
